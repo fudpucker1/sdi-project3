37e9f6fba1e84c63f9afdf06dbd528efa5e2ece6
emilygvargas <emily.vargas@spaceforce.mil>
2024-04-08 14:54:24 -0700

    added styling

--- helpdeskhero/src/App.js
+++ helpdeskhero/src/App.js
@@ -9,12 +9,14 @@ import TicketSubmission from './Pages/TicketSubmission';
 import PostSubmission from './Pages/PostSubmission';
 import TicketInfo from './Pages/TicketInfo';
 import FAQ from './Pages/FAQ';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>HelpDeskHero</h1>
-
+      <h1 className='text-center '>HelpDeskHero</h1>
+    
+    
       <nav>
         <ul>
             <li><Link to="/home">Home</Link></li>
--- helpdeskhero/src/Pages/Home.js
+++ helpdeskhero/src/Pages/Home.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function Home() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <button onClick={() => navigate('/submit-ticket')}>Submit a Ticket</button>
-      <button onClick={() => navigate('/ticket-status')}>Ticket Status</button>
-      <button onClick={() => navigate('/faq')}>FAQs</button>
-    </div>
+    <>
+    <button className="btn-lg btn btn-dark float-end" style={{ marginRight: 10}}>Log In</button>
+<br/>
+    <div style={{paddingBottom: '50%', marginTop: 20}}>
+      <button className="btn-lg btn btn-dark mt-5" style={{marginRight: '10%', marginLeft: '10%', padding: 20}} onClick={() => navigate('/submit-ticket')}>Submit a Ticket</button>
+      <button className="btn-lg btn btn-dark mt-5" style={{padding: 20}} onClick={() => navigate('/ticket-status')}>Ticket Status</button>
+      <br/>
+      <button className="btn-lg btn btn-dark mt-5" style={{ padding: 20 }} onClick={() => navigate('/faq')}>FAQs</button>
+      
+
+      </div>
+      </>
   );
 }
 
